00cbec4d494e7b79b0d224fe1ae363e70dfe31ea
DenshiNingen <denshi.ningen@gmail.com>
2025-11-18 15:51:08 +0100

    Add ESPHome app configuration to ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       ...require('./apps/proxy/pm2.config.js').apps,
       ...require('./apps/next/pm2.config.js').apps,
       ...require('./apps/admin/pm2.config.js').apps,
-      ...require('./apps/nextra/pm2.config.js').apps
+      ...require('./apps/nextra/pm2.config.js').apps,
+      ...require('./apps/esphome/pm2.config.js').apps
     ]
 };
